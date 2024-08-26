24d53e6b8afd5fed2cd3b9ca8899ee825ad596a7
Diego Cardoso Nunes <diegocar.nunes@gmail.com>
2024-08-26 09:20:09 -0300

    More tests

--- tests/test_programs.py
+++ tests/test_programs.py
@@ -26,3 +26,24 @@ def test_prog2_dynamic():
     output = run_program("tests/prog2.pd", "dynamic")
     expected = pathlib.Path("tests/prog2_out_dynamic.txt").read_text()
     assert output == expected
+
+def test_prog3_static():
+    output = run_program("tests/prog3.pd", "static")
+    expected = pathlib.Path("tests/prog3_out_static.txt").read_text()
+    assert output == expected
+
+def test_prog3_dynamic():
+    output = run_program("tests/prog3.pd", "dynamic")
+    expected = pathlib.Path("tests/prog3_out_dynamic.txt").read_text()
+    assert output == expected
+
+def test_prog4_static():
+    output = run_program("tests/prog4.pd", "static")
+    expected = pathlib.Path("tests/prog4_out_static.txt").read_text()
+    assert output == expected
+
+def test_prog4_dynamic():
+    output = run_program("tests/prog4.pd", "dynamic")
+    expected = pathlib.Path("tests/prog4_out_dynamic.txt").read_text()
+    assert output == expected
+
